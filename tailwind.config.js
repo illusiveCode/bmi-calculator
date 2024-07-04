ca5605670354a8e3903e93e5f831b2d22c7a29ab
Muhammad Raza <mh.raza2906@gmail.com>
2024-07-04 18:45:47 +0100

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,33 @@
+/** @type {import('tailwindcss').Config} */
+const tailwindConfig = {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        blue: "#345FF6",
+        gunmetal: "#253347",
+        "dark-electric-blue": "#5E6E85",
+        "pure-white": "#FFFFFF",
+      },
+      fontSize: {
+        "heading-xl": ["64px", { lineHeight: "110%" }],
+        "heading-l": ["48px", { lineHeight: "110%" }],
+        "heading-m": ["24px", { lineHeight: "110%" }],
+        "heading-s": ["20px", { lineHeight: "110%" }],
+        "body-m": ["16px", { lineHeight: "150%" }],
+        "body-m-bold": ["16px", { lineHeight: "150%" }],
+        "body-s": ["14px", { lineHeight: "150%" }],
+      },
+      fontFamily: {
+        inter: ["Inter", "sans-serif"],
+      },
+      fontWeight: {
+        "semi-bold": 600,
+        regular: 400,
+      },
+    },
+  },
+  plugins: [],
+};
+
+export default tailwindConfig;
