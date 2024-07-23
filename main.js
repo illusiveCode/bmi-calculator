be8ff2c27c60b09adda7ce2aab93e769b106d566
Muhammad Raza <mh.raza2906@gmail.com>
2024-07-23 18:03:27 +0100

    feat: add logic for different inputs

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const metricInputs = document.querySelectorAll(".metric-inputs");
+  const imperialInputs = document.querySelectorAll(".imperial-inputs");
+  const imperialRadioButton = document.getElementById("imperialRadioButton");
+  const metricRadioButton = document.getElementById("metricRadioButton");
 
+  function toggleInputFields() {
+    if (imperialRadioButton.checked) {
+      imperialInputs.forEach((input) => (input.style.display = "grid"));
+      metricInputs.forEach((input) => (input.style.display = "none"));
+    } else if (metricRadioButton.checked) {
+      metricInputs.forEach((input) => (input.style.display = "flex"));
+      imperialInputs.forEach((input) => (input.style.display = "none"));
+    }
+  }
+
+  imperialRadioButton.addEventListener("change", toggleInputFields);
+  metricRadioButton.addEventListener("change", toggleInputFields);
+
+  toggleInputFields();
+});
